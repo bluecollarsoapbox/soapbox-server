b8c4d5037faac082560ff72b470cbbb34be25249
bluecollarsoapbox <bluecollarsoapbox@gmail.com>
2025-09-10 14:05:17 -0400

    added transparent watermark

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true, limit: '2mb' }));
+app.use("/api", require("./routes/witness-s3"));
 
 // ---------- Paths / dirs ----------
 const ROOT = __dirname;
