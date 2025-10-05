fcf7ba79d837e441bedbfb1a639782ae3537a7a9
bluecollarsoapbox <bluecollarsoapbox@gmail.com>
2025-10-05 12:32:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,10 @@ if (DISCORD_TOKEN) {
 // ---------- APP ----------
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "25mb" }));
+// Increase body parser and upload limits
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ limit: "500mb", extended: true }));
+
 
 // Serve the entire Render disk at /static
 app.use("/static", express.static(DATA_ROOT, {
